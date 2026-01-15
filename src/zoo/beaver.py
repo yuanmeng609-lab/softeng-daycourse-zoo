260bfd597b6a8e9f76ffbf6d3d4e0f95a610ce0a
yuanmeng609-lab <yuanmeng609@gmail.com>
2026-01-15 14:34:43 +0000

    Beaver the Boo

--- src/zoo/beaver.py
+++ src/zoo/beaver.py
@@ -2,14 +2,20 @@
 from .animal import Animal
 
 class Beaver(Animal):
-    def__int__(self, name="Boo"):
+    def __init__(self, name="Boo"):
         super().__init__(name, species="Beaver")
 
     def sound(self):
-        return "gnaws"
+        return "Meow"
 
     def action(self):
         return "builds dams like a workaholic."
 
+def test_beaver():
+    b =Beaver("Boo")
+    assert b.name == "Boo"
+    assert b.species == "Beaver"
+    assert b.sound() == "Meow"
+    assert b.action() == "builds dams like a workaholic."
 
         
